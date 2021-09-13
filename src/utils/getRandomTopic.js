128166a282bc2428169ddd02fac702c178da0b58
Merouane Bali <merouane.bali.inbox@gmail.com>
2021-09-13 19:05:53 +0100

    Fix index of random topic
    use the full length of topics array
    
    changed index type from array containing single value to number

--- src/utils/getRandomTopic.js
+++ src/utils/getRandomTopic.js
@@ -1,7 +1,7 @@
 export default () => {
   const topics = ['Randomization', 'Abdelkader ibn Muhieddine'];
 
-  const index = [Math.floor(Math.random() * (topics.length - 1))];
+  const index = Math.floor(Math.random() * topics.length);
 
   return topics[index];
 };
