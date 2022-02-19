18a50fa8f859d8ee9c08439691e1ce97b6f1c8f8
MerouaneBali <57154963+MerouaneBali@users.noreply.github.com>
2022-02-19 13:34:32 +0100

    fix: portfolio website

--- src/utils/cldi.js
+++ src/utils/cldi.js
@@ -79,7 +79,7 @@ export default () => {
   );
 
   console.log(
-    '%cMy portfolio:%cwww.merouane-bali-official.it',
+    '%cMy portfolio:%chttps://merouane-bali.netlify.app',
     genConsoleStyles({
       color: 'white',
       background: 'black',
