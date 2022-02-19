82409ef4cd5c28b5050649e06844fe4777b37b2f
Merouane Bali <merouane.bali.inbox@gmail.com>
2022-02-19 11:35:18 +0100

    fix: add /docs to /public

---
+++ public/docs/entry.js
@@ -0,0 +1,68 @@
+
+    window.reactComponents = {};
+
+    window.vueComponents = {};
+
+  
+      import React from "react";
+
+      import ReactDOM from "react-dom";
+
+
+      import ReactWrapper from '../../node_modules/better-docs/lib/react-wrapper.js';
+
+      window.React = React;
+
+      window.ReactDOM = ReactDOM;
+
+      window.ReactWrapper = ReactWrapper;
+
+    
+    import './styles/reset.css';
+
+    import './styles/iframe.css';
+
+  import Component0 from '../../src/components/AboutTerminal.jsx';
+reactComponents['AboutTerminal'] = Component0;
+
+import Component1 from '../../src/components/GenerateTestTerminal.jsx';
+reactComponents['GenerateTestTerminal'] = Component1;
+
+import Component2 from '../../src/components/Header.jsx';
+reactComponents['Header'] = Component2;
+
+import Component3 from '../../src/components/Input.jsx';
+reactComponents['Input'] = Component3;
+
+import Component4 from '../../src/components/Menu.jsx';
+reactComponents['Menu'] = Component4;
+
+import Component5 from '../../src/components/MenuButton.jsx';
+reactComponents['MenuButton'] = Component5;
+
+import Component6 from '../../src/components/MenuCheckbox.jsx';
+reactComponents['MenuCheckbox'] = Component6;
+
+import Component7 from '../../src/components/MenuInput.jsx';
+reactComponents['MenuInput'] = Component7;
+
+import Component8 from '../../src/components/MenuSelect.jsx';
+reactComponents['MenuSelect'] = Component8;
+
+import Component9 from '../../src/components/RoadMapTerminal.jsx';
+reactComponents['RoadMapTerminal'] = Component9;
+
+import Component10 from '../../src/components/SearchTopicTerminal.jsx';
+reactComponents['SearchTopicTerminal'] = Component10;
+
+import Component11 from '../../src/components/Terminal.jsx';
+reactComponents['Terminal'] = Component11;
+
+import Component12 from '../../src/components/Test.jsx';
+reactComponents['Test'] = Component12;
+
+import Component13 from '../../src/components/TestEndDialogTerminal.jsx';
+reactComponents['TestEndDialogTerminal'] = Component13;
+
+import Component14 from '../../src/components/TestTerminal.jsx';
+reactComponents['TestTerminal'] = Component14;
